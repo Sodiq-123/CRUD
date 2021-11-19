f21eb05a97bc30d3cb7762b3a230dea4f8cc688f
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2021-11-19 03:34:37 +0100

    Modified

--- routes/api.js
+++ routes/api.js
@@ -40,7 +40,7 @@ router.post('/todos', async (req, res) => {
       todo: null
       })
     }
-    return res.status(200).json({
+    return res.status(201).json({
       success: true,
       message: 'Successfully created Todo',
       todo: todos
